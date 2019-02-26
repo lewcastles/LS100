96c53ad5095e7938ba40e4b98fb9a1da06c94492
Lewis Castles <castles.lewis@gmail.com>
2019-02-25 22:50:47 -0800

    Work on LS100 2-25-2019

---
+++ Basics/no3.rb
@@ -0,0 +1,14 @@
+#Write a program that uses a hash to store a list of movie 
+#titles with the year they came out. Then use the puts command to make 
+#your program print out the year of each movie to the screen. 
+#The output for your program should look something like this.
+
+movie_list = {lion_king: 1994, 
+              toy_story_2: 1999, 
+              the_matrix: 1999,
+              rain_man: 1988}
+
+puts movie_list[:lion_king]
+puts movie_list[:toy_story_2]
+puts movie_list[:the_matrix]
+puts movie_list[:rain_man]
---
+++ Basics/no4.rb
@@ -0,0 +1,5 @@
+dates = [1994, 1999, 1999, 1988]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
