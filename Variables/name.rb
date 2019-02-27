ec62c4b1698a7d22ecf3bf01132e7ec898c66dca
Lewis Castles <castles.lewis@gmail.com>
2019-02-26 19:45:56 -0800

    age and name examples LS100

---
+++ Variables/name.rb
@@ -0,0 +1,10 @@
+puts "Please enter your first name..."
+first_name = gets.chomp
+puts "Please enter your last name..."
+last_name = gets.chomp
+puts "Hello #{first_name} #{last_name}, Welcome!"
+
+# Asks the user for their first name, saves it into a variable, 
+#and then does the same for the last name. 
+# Then outputs their full name all at once.
+
